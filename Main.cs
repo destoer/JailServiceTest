bdc533ac87e3d2e1505969fcd437112f558d5cee
destoer <destoer1234@protonmail.com>
2024-04-22 21:43:51 +0100

    initial commit

---
+++ Main.cs
@@ -0,0 +1,42 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Cvars;
+using CounterStrikeSharp.API.Modules.Entities;
+using CounterStrikeSharp.API.Core.Capabilities;
+using CounterStrikeSharp.API.Modules.Commands;
+
+public class JailServiceTestPlugin : BasePlugin
+{
+    public override string ModuleName => "Hello World Plugin";
+
+    public override string ModuleVersion => "0.0.1";
+
+    public override void Load(bool hotReload)
+    {
+        AddCommand("test_warden","test warden",TestWarden);
+    }
+
+    public static PluginCapability<IWardenService> wardenService {get; } = new ("jailbreak:warden_service");
+
+    public static void TestWarden(CCSPlayerController? invoke, CommandInfo command)
+    {
+        if(invoke == null || !invoke.IsValid)
+        {
+            return;
+        }
+
+        var service = wardenService.Get();
+
+        if(service != null)
+        {
+            invoke.PrintToChat($"are you a warden?: {service.IsWarden(invoke)}");
+        }
+
+        else
+        {
+            invoke.PrintToChat("no service");
+        }
+    }
+}
---
+++ install.sh
@@ -0,0 +1,2 @@
+yes | cp bin/Debug/net8.0/* "../../plugins/JailServiceTest/"
+#cp -r lang "../../plugins/JailServiceTest/"
